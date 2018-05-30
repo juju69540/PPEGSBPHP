5aac933e0e4819bfc2ae409f552008ea46d6e7a8
Unknown <juju692010@live.fr>
2018-05-30 17:34:19 +0200

    Delete error of Git <<<<<Dev""

--- controleurs/c_visiteur.php
+++ controleurs/c_visiteur.php
@@ -1,22 +1,3 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
-<?php
-include("vues/v_sommaire.php");
-$action = $_REQUEST['action'];
-
-switch($action){
-	case 'lesVisiteurs':{
-	$selectVisiteurs= $pdo->getLesVisiteurs();
-	include("vues/v_visiteur.php");
-	break;
-	}
-	
-	case 'ajouterVisiteurs':{
-	include ("vues/v_ajouterVisiteur.php");
-	break;
-	}
-}
-=======
 <?php
 include("vues/v_sommaire.php");
 $action = $_REQUEST['action'];
@@ -28,18 +9,4 @@ switch($action){
 	break;
 	}
 }
->>>>>>> devJulien
-=======
-<?php
-include("vues/v_sommaire.php");
-$action = $_REQUEST['action'];
-
-switch($action){
-	case 'lesVisiteurs':{
-	$selectVisiteurs= $pdo->getLesVisiteurs();
-	include("vues/v_visiteur.php");
-	break;
-	}
-}
->>>>>>> devJulian
 ?>
--- vues/v_visiteur.php
+++ vues/v_visiteur.php
@@ -1,68 +1,7 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
-<div id="contenu">
-	<h2>Les visiteurs</h2>
-	<form name="afficher" action="v_visiteur.php" method="post">
-	<table>
-		<tr>
-			<th></th>
-			<th>ID</th>
-			<th>Nom</th>
-			<th>Prenom</th>
-			<th>Login</th>
-			<th>Mdp</th>
-			<th>Adresse</th>
-			<th>Code Postal</th>
-			<th>Ville</th>
-			<th>Date d'embauche</th>
-		</tr>
-		<tr>
-			<?php
-				foreach ($selectVisiteurs as $unVisiteur)
-					{
-					$unID = $unVisiteur['id'];
-					$unNom = $unVisiteur['nom'];
-					$unPrenom = $unVisiteur['prenom'];
-					$unLogin = $unVisiteur['login'];
-					$unMdp = $unVisiteur['mdp'];
-					$uneAdresse = $unVisiteur['adresse'];
-					$unCP = $unVisiteur['cp'];
-					$uneVille = $unVisiteur['ville'];
-					$uneDateE = $unVisiteur['dateEmbauche'];
-			?>
-			<tr>
-				<td><input type="checkbox" name="selectionner" value="cocher"></td>
-				<td><?php echo $unID ?></td>
-				<td><?php echo $unNom ?></td>
-				<td><?php echo $unPrenom ?></td>
-				<td><?php echo $unLogin ?></td>
-				<td><?php echo $unMdp ?></td>
-				<td><?php echo $uneAdresse ?></td>
-				<td><?php echo $unCP ?></td>
-				<td><?php echo $uneVille ?></td>
-				<td><?php echo $uneDateE ?> </td>
-			</tr>
-				<?php
-					}
-				?>
-		</tr>
-	</table>
-	<input type="button" name="modifier" value="Modifier">
-	<input type="button" name="supprimer" value="Supprimer">
-	<a href="index.php?uc=visiteur&action=ajouterVisiteurs" title="ajouterVisiteurs"><input type="button" name="ajouter" value="Ajouter"></a>
-	</form>
-	
-=======
-<div class="container">
-	<div id="contenu">
-		<h2>Les visiteurs</h2>
-		<table class="table table-borderless">
-=======
 <div class="container">
 	<div id="contenu">
 		<h2>Les visiteurs</h2>
 		<table class="table table-borderless" style="margin-left:5px;">
->>>>>>> devJulian
 			<thead class="thead-light">
 				<tr>
 					<th></th>
@@ -110,10 +49,5 @@
 			</table>
 		<INPUT type="button" name="nom" value="Modifier">
 		<INPUT type="button" name="nom" value="Supprimer">
-<<<<<<< HEAD
 	</div>
->>>>>>> devJulien
-=======
-	</div>
->>>>>>> devJulian
 </div>
